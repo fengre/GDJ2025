0595eab755dee5872635aadfb3c1ee061a03a509
Annie Liu <al4289@princeton.edu>
2025-04-16 22:13:58 -0400

    pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+    public AudioSource musicSource;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if (GameIsPaused){
+                Resume();
+            }else{
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        musicSource.UnPause();
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        musicSource.Pause();
+        GameIsPaused = true;
+    }
+
+    public void LoadSettings()
+    {
+        Debug.Log("loading settings");
+        //SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game..");
+        Application.Quit();
+    }
+}
